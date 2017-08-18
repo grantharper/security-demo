755503c05cfd6644a797967d736607ab0d477e4f
grantharper <gharper3@gmail.com>
2017-08-17 21:28:33 -0400

    BankAccount domain object added

--- src/main/java/org/grantharper/websecurity/domain/BankAccount.java
+++ src/main/java/org/grantharper/websecurity/domain/BankAccount.java
@@ -20,6 +20,30 @@ public class BankAccount {
 	private Long customerId;
 	
 	private Double balance;
+
+	public Long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(Long accountId) {
+		this.accountId = accountId;
+	}
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
 	
 	
 	
